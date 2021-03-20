2e4222cc90cb849d4c2dd3d53caa997d4adf159b
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2021-03-20 19:27:16 +0100

    multiple angular step forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-multiple-step-form';
+  title = 'anular multiple step form';
+
+  isLinear = true;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.firstFormGroup = this._formBuilder.group({
+      name: "",
+      description: ""
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      amount: "",
+      stock: ""
+    });
+  }
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      amount: ['', Validators.required],
+      stock: ['', Validators.required]
+    });
+  }
+
+  submit(){
+      console.log(this.firstFormGroup.value);
+      console.log(this.secondFormGroup.value);
+  }
 }
